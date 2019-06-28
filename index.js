70408e435ff7030acc1b144443bc4fe6563f01ed
CodingOni <37724661+CodingOni@users.noreply.github.com>
2019-06-28 16:21:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
+// Set a Static Folder
+app.use(express.static(path.join(__dirname,'public')));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
